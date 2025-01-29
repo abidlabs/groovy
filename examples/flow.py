aa5a3d82fe49c0c22a7e4c78c899745ddd181943
Abubakar Abid <abubakar@huggingface.co>
2025-01-29 00:51:22 -0800

    changes

--- examples/flow.py
+++ examples/flow.py
@@ -2,7 +2,13 @@ import gradio as gr
 
 import groovy as gv
 
-flow = gv.Flow("navigate to {}", [gr.Textbox("hf.co")])
+flow = gv.Flow(
+    task="Find me some events in {} related to {}.",
+    inputs=[
+        gr.Dropdown(["San Francisco", "New York", "Chicago", "Los Angeles", "Boston"], allow_custom_value=True),
+        gr.Dropdown(["board games", "cooking", "hiking", "reading", "writing"], allow_custom_value=True),
+    ],
+)
 
 if __name__ == "__main__":
     flow.launch()
